484a3cab8aec35cc4d5868e3ef073000b285b304
takewiki <hulilei@takewiki.com.cn>
2023-07-05 13:12:43 +0800

    v2.1

--- R/overDueList.R
+++ R/overDueList.R
@@ -24,6 +24,29 @@ return(res)
 }
 
 
+#' 产成品逾期查询
+#'
+#' @param token 口令
+#' @param FCategory 产品分组
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' overDuePrd_query_cn()
+overDuePrd_query_cn <- function(token='D62212AC-5651-4387-B3AC-42D3E38D6AFD',FCategory=NULL) {
+
+
+
+  res = overDuePrd_query(token = token,FCategory = FCategory)
+  ncount = nrow(res)
+  if(ncount >0){
+    names(res) <- c('产品分组','计划交货日期','逾期天数','采购订单号','物料名称','物料编码','负责人','客户','数量','生产日期')
+  }
+  return(res)
+}
+
+
 #' 产品逾期将视图数据插入对应表
 #'
 #' @param token 口令
@@ -75,6 +98,26 @@ overDueSParts_query <- function(token='D62212AC-5651-4387-B3AC-42D3E38D6AFD') {
 
   sql <- paste0("select * from  rds_vm_vw_std_overDueListSParts order by Category,plannedshipDate")
   res <-tsda::sql_select2(token = token,sql = sql)
+  return(res)
+}
+
+#' 备件逾期表查询数据
+#'
+#' @param token 口令
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' overDueSParts_query_cn()
+overDueSParts_query_cn <- function(token='D62212AC-5651-4387-B3AC-42D3E38D6AFD') {
+
+
+  res <-overDueSParts_query(token = token)
+  ncount =nrow(res)
+  if(ncount >0){
+    names(res) <-c('产品分组','计划交期','逾期天数','采购订单号','物料名称','物料代码','产线','客户','数量')
+  }
   return(res)
 }
 
